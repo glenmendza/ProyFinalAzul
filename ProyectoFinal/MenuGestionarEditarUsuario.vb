b46823ebe668a3c3986aca34d9004b190ed45aa4
Haziel <hlopezr24@gmail.com>
2021-08-05 19:14:47 -0600

    Prueba de cambios de form

--- ProyectoFinal/MenuGestionarEditarUsuario.vb
+++ ProyectoFinal/MenuGestionarEditarUsuario.vb
@@ -1,6 +1,11 @@
 ﻿Public Class MenuGestionarEditarUsuario
+    'prueba 
     Private Sub ButtonBack_Click(sender As Object, e As EventArgs) Handles ButtonBack.Click
         Me.Hide()
         MenuBuscarUsuario.Show()
+    End Sub
+
+    Private Sub MenuGestionarEditarUsuario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
     End Sub
 End Class
