c46a6cc9d9baa282922c302d602e9c8cbf5e5531
Haziel <hlopezr24@gmail.com>
2021-08-19 21:39:30 -0600

    Cambios Haziel

--- ProyectoFinal/MenuAgenda.vb
+++ ProyectoFinal/MenuAgenda.vb
@@ -1,32 +1,38 @@
 ﻿Imports System.Data
 Imports System.Data.SqlClient
 Public Class MenuAgenda
-
+    Dim contador As Integer = 1
+    Dim contador2 As Integer = contador
     Public Function Insertar()
 
         Dim sCon As String = "data source=PROYAZUL; initial catalog = Gym; Integrated Security = True"
         Dim sel As String
         Dim NombreTabla As String = "Agenda"
-
         sel = "INSERT INTO " & NombreTabla &
-            " (Cedula,Fecha, Notas) " &
+            " (Cedula,Fecha,NumNota, Notas) " &
             "VALUES " &
-            "(@Cedula,@Fecha, @Notas)"
+            "(@Cedula,@Fecha, @NumNota, @Notas)"
 
         Using con As New SqlConnection(sCon)
 
             Dim cmd As New SqlCommand(sel, con)
+
             cmd.Parameters.AddWithValue("@Cedula", cedula)
             cmd.Parameters.AddWithValue("@Fecha", CalendarioAgenda.SelectionStart)
             cmd.Parameters.AddWithValue("@Notas", TextBoxNota.Text)
+            cmd.Parameters.AddWithValue("@NumNota", contador)
 
-            'añadir la opcion de agregar una nueva nota en el mismo dia 
+            Try
+                con.Open()
+                Dim t As Date = CDate(cmd.ExecuteScalar())
+                con.Close()
+                Return t
+            Catch
+                MsgBox("Una nota ya fue almacenada en esta posicion")
+            End Try
 
-            'Si se almacena una nota con la misma cedula, manda error 
-            con.Open()
-            Dim t As Date = CDate(cmd.ExecuteScalar())
-            con.Close()
-            Return t
+
+
         End Using
     End Function
     Private Sub Button7_Click(sender As Object, e As EventArgs) Handles ButtonBack.Click
@@ -34,64 +40,58 @@ Public Class MenuAgenda
         MenuPrincipal.Show()
     End Sub
 
-
-
-    Private Sub CalendarioAgenda_DateChanged(sender As Object, e As DateRangeEventArgs) Handles CalendarioAgenda.DateChanged
-
-    End Sub
-
-    Private Sub MenuAgenda_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
     Private Sub CalendarioAgenda_DateSelected(sender As Object, e As DateRangeEventArgs) Handles CalendarioAgenda.DateSelected
-
         Label2.Text = CalendarioAgenda.SelectionStart
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles ButtonNota.Click
+        TextBoxNota.Text = " "
+        TextBoxNota.ReadOnly = False
         TextBoxNota.Show()
         ButtonGuardarNota.Show()
     End Sub
 
-    Private Sub TextBoxNota_TextChanged(sender As Object, e As EventArgs) Handles TextBoxNota.TextChanged
 
-    End Sub
 
     Private Sub ButtonGuardarNota_Click(sender As Object, e As EventArgs) Handles ButtonGuardarNota.Click
         Call Insertar()
+        contador = contador + 1
         MsgBox("Su nota fue almacenada correctamente", MessageBoxIcon.Information, "FitTracker")
     End Sub
 
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButtonConsultar.Click
-        Dim contador As Integer
+
         Dim constr As String = ("data source=PROYAZUL; initial catalog = Gym; Integrated Security = True”)
 
         Using con As SqlConnection = New SqlConnection(constr)
-            Using cmd As SqlCommand = New SqlCommand("SELECT Cedula, Notas, CONVERT(varchar, Fecha, 101) FROM Agenda WHERE (Cedula = " & cedula & ") and (Fecha = '" & CalendarioAgenda.SelectionStart & "' )")
+            Using cmd As SqlCommand = New SqlCommand("SELECT Cedula, Notas, CONVERT(varchar, Fecha, 101), NumNota FROM Agenda WHERE (Cedula = " & cedula & "), (NumNota = )" & contador2 & " and (Fecha = '" & CalendarioAgenda.SelectionStart & "' )")
 
                 cmd.CommandType = CommandType.Text
                 cmd.Connection = con
                 con.Open()
+                Try
+                    Using lector As SqlDataReader = cmd.ExecuteReader()
 
-                Using lector As SqlDataReader = cmd.ExecuteReader()
-
-                    lector.Read()
-
-                    While lector.Read()
-                        contador = contador + 1
-                    End While
-                    MsgBox(contador)
-                    MsgBox("Sus notas del dia son: " + lector("Notas"), MessageBoxIcon.Information, "Fit Tracker")
+                        lector.Read()
 
 
-                End Using
+                        TextBoxNota.Multiline = True
+                        TextBoxNota.ReadOnly = True
+                        TextBoxNota.Text =
+                            "Sus notas del dia son: " & vbCrLf & lector("Notas")
+                        TextBoxNota.Show()
+                    End Using
+                Catch
 
+                    MsgBox("No tiene notas almacenadas en este dia", MessageBoxIcon.Error, "FitTracker")
+                End Try
                 con.Close()
 
             End Using
         End Using
 
     End Sub
+
+
 End Class
