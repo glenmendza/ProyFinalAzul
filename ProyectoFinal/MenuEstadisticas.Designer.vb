44ee29a22be3d90f16b768f6ef9d5d6f1e34ecd5
glenmendza <glenmendoza10@gmail.com>
2021-08-19 21:59:53 -0600

    adsad

--- ProyectoFinal/MenuEstadisticas.Designer.vb
+++ ProyectoFinal/MenuEstadisticas.Designer.vb
@@ -26,6 +26,7 @@ Partial Class MenuEstadisticas
         Me.ButtonBack = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -35,10 +36,9 @@ Partial Class MenuEstadisticas
         Me.ButtonBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.ButtonBack.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ButtonBack.ForeColor = System.Drawing.Color.White
-        Me.ButtonBack.Location = New System.Drawing.Point(16, 15)
-        Me.ButtonBack.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ButtonBack.Location = New System.Drawing.Point(12, 12)
         Me.ButtonBack.Name = "ButtonBack"
-        Me.ButtonBack.Size = New System.Drawing.Size(69, 46)
+        Me.ButtonBack.Size = New System.Drawing.Size(52, 37)
         Me.ButtonBack.TabIndex = 18
         Me.ButtonBack.Text = "←"
         Me.ButtonBack.UseVisualStyleBackColor = False
@@ -49,33 +49,41 @@ Partial Class MenuEstadisticas
         Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(22, Byte), Integer), CType(CType(32, Byte), Integer), CType(CType(42, Byte), Integer))
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(508, 57)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Location = New System.Drawing.Point(381, 46)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(302, 46)
+        Me.Label1.Size = New System.Drawing.Size(242, 37)
         Me.Label1.TabIndex = 20
         Me.Label1.Text = "Mis estadísticas"
         '
         'PictureBox1
         '
-        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(340, 223)
+        Me.PictureBox1.Location = New System.Drawing.Point(255, 181)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(694, 352)
+        Me.PictureBox1.Size = New System.Drawing.Size(520, 286)
         Me.PictureBox1.TabIndex = 21
         Me.PictureBox1.TabStop = False
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(479, 526)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 22
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'MenuEstadisticas
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(22, Byte), Integer), CType(CType(32, Byte), Integer), CType(CType(42, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(1359, 794)
+        Me.ClientSize = New System.Drawing.Size(1019, 645)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.ButtonBack)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "MenuEstadisticas"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -87,4 +95,5 @@ Partial Class MenuEstadisticas
     Private WithEvents ButtonBack As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
 End Class
--- ProyectoFinal/MenuEstadisticas.vb
+++ ProyectoFinal/MenuEstadisticas.vb
@@ -7,7 +7,7 @@ Public Class MenuEstadisticas
     Dim conn As SqlConnection
     Dim COMMAND As SqlCommand
     Dim dbDataSet As New DataSet
-
+    'Public Property Chart1 As Object
 
     Private Sub ButtonBack_Click(sender As Object, e As EventArgs) Handles ButtonBack.Click
         Me.Hide()
@@ -15,33 +15,30 @@ Public Class MenuEstadisticas
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        conn = New SqlConnection
-        conn.ConnectionString = "data source=PROYAZUL; initial catalog = Gym; Integrated Security = True”
-        Dim READER As SqlDataReader
-        Try
-            conn.Open()
-            Dim Query As String
-            Query = "SELECT * FROM dbo.Usuarios"
-            COMMAND = New SqlCommand(Query, conn)
-            READER = Command.ExecuteReader
-            While READER.Read
+        'conn = New SqlConnection
+        'conn.ConnectionString = "data source=PROYAZUL; initial catalog = Gym; Integrated Security = True”
+        'Dim READER As SqlDataReader
+        'Try
+        '    conn.Open()
+        '    Dim Query As String
+        '    Query = "SELECT * FROM dbo.Usuarios"
+        '    COMMAND = New SqlCommand(Query, conn)
+        '    READER = Command.ExecuteReader
+        '    While READER.Read
 
-                Chart1.Series("Peso").Points.AddXY(READER("Nombre"), READER("Peso"))
+        '        Chart1.Series("Peso").Points.AddXY(READER("Nombre"), READER("Peso"))
 
-            End While
-            conn.Close()
+        '    End While
+        '    conn.Close()
 
-        Catch ex As SqlException
-            MessageBox.Show(ex.Message)
-        Finally
-            conn.Dispose()
-        End Try
+        'Catch ex As SqlException
+        '    MessageBox.Show(ex.Message)
+        'Finally
+        '    conn.Dispose()
+        'End Try
     End Sub
 
     Private Sub MenuEstadisticas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-
-
 
     End Sub
 End Class
