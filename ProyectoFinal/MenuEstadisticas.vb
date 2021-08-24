4650886f317e774c971b52444b396cc8b1855dca
glenmendza <glenmendoza10@gmail.com>
2021-08-24 15:55:25 -0600

    Revert "Modificacion a Form Estadisticas"
    This reverts commit a45a11dcf9a90caae1a625cbc3f23f38c05a742f.

--- ProyectoFinal/MenuEstadisticas.vb
+++ ProyectoFinal/MenuEstadisticas.vb
@@ -14,7 +14,6 @@ Public Class MenuEstadisticas
         MenuPrincipal.Show()
     End Sub
 
-<<<<<<< Updated upstream
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'conn = New SqlConnection
         'conn.ConnectionString = "data source=PROYAZUL; initial catalog = Gym; Integrated Security = True”
@@ -37,41 +36,9 @@ Public Class MenuEstadisticas
         'Finally
         '    conn.Dispose()
         'End Try
-=======
-    Private Sub Button1_Click(sender As Object, e As EventArgs)
-
->>>>>>> Stashed changes
     End Sub
 
     Private Sub MenuEstadisticas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-<<<<<<< Updated upstream
-=======
-        conn = New SqlConnection
-        conn.ConnectionString = "data source=PROYAZUL; initial catalog = Gym; Integrated Security = True”
-        Dim READER As SqlDataReader
-
-
-        Try
-            conn.Open()
-            Dim Query As String
-            Query = "SELECT Peso, Nombre FROM dbo.Usuarios WHERE Cedula = " + cedula
-            COMMAND = New SqlCommand(Query, conn)
-            READER = COMMAND.ExecuteReader
-            While READER.Read
-
-                Chart1.Series("Peso").Points.AddXY(READER("Nombre"), READER("Peso"))
-
-            End While
-            conn.Close()
-
-        Catch ex As SqlException
-            MessageBox.Show(ex.Message)
-        Finally
-            conn.Dispose()
-        End Try
-
-
->>>>>>> Stashed changes
     End Sub
 End Class
