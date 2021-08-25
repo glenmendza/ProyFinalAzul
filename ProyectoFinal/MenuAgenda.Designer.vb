6cbfa77dfa06d8daa7ac16955bef72c4656e4dac
Haziel <hlopezr24@gmail.com>
2021-08-25 13:11:55 -0600

    Agenda Final

--- ProyectoFinal/MenuAgenda.Designer.vb
+++ ProyectoFinal/MenuAgenda.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class MenuAgenda
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class MenuAgenda
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MenuAgenda))
         Me.ButtonBack = New System.Windows.Forms.Button()
@@ -111,7 +111,7 @@ Partial Class MenuAgenda
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
         Me.Label2.ImageAlign = System.Drawing.ContentAlignment.BottomLeft
-        Me.Label2.Location = New System.Drawing.Point(586, 377)
+        Me.Label2.Location = New System.Drawing.Point(616, 377)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(251, 29)
         Me.Label2.TabIndex = 27
@@ -122,7 +122,7 @@ Partial Class MenuAgenda
         '
         Me.TextBoxNota.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TextBoxNota.Location = New System.Drawing.Point(464, 499)
-        Me.TextBoxNota.MaxLength = 150
+        Me.TextBoxNota.MaxLength = 200
         Me.TextBoxNota.Name = "TextBoxNota"
         Me.TextBoxNota.Size = New System.Drawing.Size(453, 157)
         Me.TextBoxNota.TabIndex = 28
--- ProyectoFinal/MenuAgenda.vb
+++ ProyectoFinal/MenuAgenda.vb
@@ -1,38 +1,76 @@
 ﻿Imports System.Data
 Imports System.Data.SqlClient
 Public Class MenuAgenda
-    Dim contador As Integer = 1
-    Dim contador2 As Integer = contador
-    Public Function Insertar()
+    Dim contador As Integer
 
+    Dim connectionString As String = "data source=PROYAZUL; initial catalog = Gym; Integrated Security = True"
+    Dim connection As SqlConnection
+    Dim command As SqlCommand
+    Dim lector2 As SqlDataReader
+
+    Public Function ConectorDatos()
+        connection = New SqlConnection(connectionString)
+        command = connection.CreateCommand()
+        command.CommandType = System.Data.CommandType.Text
+    End Function
+
+
+    Public Function Verificar()
+        command.CommandText = "SELECT MAX(NumNota) AS NumeroMayor FROM Agenda WHERE Cedula = " & cedula & " AND (Fecha = '" & CalendarioAgenda.SelectionStart & "')"
+        Using (connection)
+            connection.Open()
+            lector2 = command.ExecuteReader()
+            lector2.Read()
+            If IsDBNull(lector2.Item("NumeroMayor")) Then
+                contador = 0
+
+            ElseIf lector2.HasRows Then
+
+                contador = lector2.GetInt32(0)
+
+
+
+            End If
+
+        End Using
+
+        contador = contador + 1
         Dim sCon As String = "data source=PROYAZUL; initial catalog = Gym; Integrated Security = True"
         Dim sel As String
         Dim NombreTabla As String = "Agenda"
-        sel = "INSERT INTO " & NombreTabla &
-            " (Cedula,Fecha,NumNota, Notas) " &
-            "VALUES " &
-            "(@Cedula,@Fecha, @NumNota, @Notas)"
 
-        Using con As New SqlConnection(sCon)
+        Using con As SqlConnection = New SqlConnection(sCon)
+            sel = "INSERT INTO " & NombreTabla &
+                " (Cedula,Fecha,NumNota, Notas) " &
+                "VALUES " &
+                "(@Cedula,@Fecha, @NumNota, @Notas)"
 
-            Dim cmd As New SqlCommand(sel, con)
+            If contador = 6 Then
+                MsgBox("Ha almacenado la cantidad limite de notas por dia", MessageBoxIcon.Warning, "FitTracker")
+                Exit Function
+            Else
+                Dim cmd As New SqlCommand(sel, con)
 
-            cmd.Parameters.AddWithValue("@Cedula", cedula)
-            cmd.Parameters.AddWithValue("@Fecha", CalendarioAgenda.SelectionStart)
-            cmd.Parameters.AddWithValue("@Notas", TextBoxNota.Text)
-            cmd.Parameters.AddWithValue("@NumNota", contador)
-
-            Try
-                con.Open()
-                Dim t As Date = CDate(cmd.ExecuteScalar())
-                con.Close()
-                Return t
-            Catch
-                MsgBox("Una nota ya fue almacenada en esta posicion")
-            End Try
+                cmd.Parameters.AddWithValue("@Cedula", cedula)
+                cmd.Parameters.AddWithValue("@Fecha", CalendarioAgenda.SelectionStart)
+                cmd.Parameters.AddWithValue("@Notas", TextBoxNota.Text)
+                cmd.Parameters.AddWithValue("@NumNota", contador)
 
 
+                Try
+                    con.Open()
+                    Dim t As Date = CDate(cmd.ExecuteScalar())
+                    MsgBox("Su nota fue almacenada correctamente. Número de nota : " & contador, MessageBoxIcon.Information, "FitTracker")
+                    TextBoxNota.Text = " "
+                    con.Close()
+                    Return t
+                Catch
+                    MsgBox("Ya no puede almacenar mas notas en este dia", MessageBoxIcon.Warning, "Fit Tracker")
+                    Exit Function
+                End Try
 
+
+            End If
         End Using
     End Function
     Private Sub Button7_Click(sender As Object, e As EventArgs) Handles ButtonBack.Click
@@ -54,37 +92,37 @@ Public Class MenuAgenda
 
 
     Private Sub ButtonGuardarNota_Click(sender As Object, e As EventArgs) Handles ButtonGuardarNota.Click
-        Call Insertar()
-        contador = contador + 1
-        MsgBox("Su nota fue almacenada correctamente", MessageBoxIcon.Information, "FitTracker")
+        Call ConectorDatos()
+        Call Verificar()
+
     End Sub
 
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButtonConsultar.Click
-
+        TextBoxNota.Text = " "
         Dim constr As String = ("data source=PROYAZUL; initial catalog = Gym; Integrated Security = True”)
 
         Using con As SqlConnection = New SqlConnection(constr)
-            Using cmd As SqlCommand = New SqlCommand("SELECT Cedula, Notas, CONVERT(varchar, Fecha, 101), NumNota FROM Agenda WHERE (Cedula = " & cedula & "), (NumNota = )" & contador2 & " and (Fecha = '" & CalendarioAgenda.SelectionStart & "' )")
+            Using cmd As SqlCommand = New SqlCommand("SELECT Cedula, Notas, NumNota, CONVERT(varchar, Fecha, 101), NumNota FROM Agenda WHERE (Cedula = " & cedula & ") and (Fecha = '" & CalendarioAgenda.SelectionStart & "' )")
 
                 cmd.CommandType = CommandType.Text
                 cmd.Connection = con
                 con.Open()
+
                 Try
                     Using lector As SqlDataReader = cmd.ExecuteReader()
 
-                        lector.Read()
-
-
                         TextBoxNota.Multiline = True
                         TextBoxNota.ReadOnly = True
-                        TextBoxNota.Text =
-                            "Sus notas del dia son: " & vbCrLf & lector("Notas")
-                        TextBoxNota.Show()
+                        While lector.Read()
+
+                            TextBoxNota.Text &= "Nota: " & lector("Notas").ToString() & vbCrLf
+                            TextBoxNota.Show()
+                        End While
                     End Using
                 Catch
 
-                    MsgBox("No tiene notas almacenadas en este dia", MessageBoxIcon.Error, "FitTracker")
+                    MsgBox("No fue posible obtener las notas del dia", MessageBoxIcon.Error, "FitTracker")
                 End Try
                 con.Close()
 
